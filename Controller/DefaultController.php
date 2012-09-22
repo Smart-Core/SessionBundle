0dc8d9fa24b2debb19c57f03f5aa6702d51f4751
Smart-Core <artem.digi@gmail.com>
2012-09-22 21:48:23 +0700

    first commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SmartCore\Bundle\SessionBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('SmartCoreSessionBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ Listener/UserId.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SmartCore\Bundle\SessionBundle\Listener;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class UserId
+{
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+    
+    public function updateSessionUserId(FilterResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST === $event->getRequestType()
+            and $this->container->has('security.context')
+            and method_exists($this->container->get('security.context')->getToken()->getUser(), 'getId')
+        ) {
+            $user_id = $this->container->get('security.context')->getToken()->getUser()->getId();
+            $this->container->get('smart_core_session.handler')->setUserId($user_id);
+        }
+    }
+}
