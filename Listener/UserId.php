d4aaf0e6ba35fec7d3a5e82f2852fe0bfec6e92e
Smart-Core <artem.digi@gmail.com>
2015-07-04 17:59:51 +0600

    replace deprecated  security.context to recommended services

--- Listener/UserId.php
+++ Listener/UserId.php
@@ -3,7 +3,7 @@
 namespace SmartCore\Bundle\SessionBundle\Listener;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
@@ -19,11 +19,11 @@ class UserId
     public function updateSessionUserId(FilterResponseEvent $event)
     {
         if (HttpKernelInterface::MASTER_REQUEST === $event->getRequestType()
-            and $this->container->has('security.context')
-            and $this->container->get('security.context')->getToken() instanceof TokenInterface
-            and method_exists($this->container->get('security.context')->getToken()->getUser(), 'getId')
+            and $this->container->has('security.token_storage')
+            and $this->container->get('security.token_storage')->getToken() instanceof TokenStorageInterface
+            and method_exists($this->container->get('security.token_storage')->getToken()->getUser(), 'getId')
         ) {
-            $user_id = $this->container->get('security.context')->getToken()->getUser()->getId();
+            $user_id = $this->container->get('security.token_storage')->getToken()->getUser()->getId();
             $this->container->get('smart_core_session.handler')->setUserId($user_id);
         }
     }
